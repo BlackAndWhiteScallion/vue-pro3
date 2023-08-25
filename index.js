49fe54115a7e21cbac1aab60a502f6e00f8b55a2
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-08-25 09:38:16 +0800

    moved folders

--- index.js
+++ index.js
@@ -30,6 +30,41 @@ new Vue({
         return 'q'
       }
     },
+    undo() {
+      this.chess.game.undo()
+      this.chess.board.set({
+        fen: this.chess.game.fen(),
+        movable: {events: { after: this.userPlay()}},
+      });
+    },
+    userPlay() {
+      return (orig, dest) => {
+        if (this.chess.isPromotion(orig, dest)) {
+          this.chess.promoteTo = this.promote();
+        }
+        this.chess.game.move({from: orig, to: dest, promotion: this.chess.promoteTo}) // promote to queen for simplicity
+        this.chess.board.set({
+          fen: this.chess.game.fen()
+        })
+        this.chess.calculatePromotions()
+        this.aiNextMove()
+      };
+    },
+    aiNextMove() {
+      let moves = this.chess.game.moves({verbose: true})
+      let randomMove = moves[Math.floor(Math.random() * moves.length)]
+      this.chess.game.move(randomMove)
+
+      this.chess.board.set({
+        fen: this.chess.game.fen(),
+        turnColor: this.chess.toColor(),
+        movable: {
+          color: this.chess.toColor(),
+          dests: this.chess.possibleMoves(),
+          events: { after: this.userPlay()},
+        }
+      });
+    },
   },
   created() {
     this.fens = ['5rr1/3nqpk1/p3p2p/Pp1pP1pP/2pP1PN1/2P1Q3/2P3P1/R4RK1 b - f3 0 28',
@@ -38,5 +73,11 @@ new Vue({
                 'rnbqkbnr/p4ppp/8/1pP1p3/1Pp1P3/8/P4PPP/RNBQKBNR w KQkq - 0 6'
                 ]
     console.log(this);
-  }
+  },
+  mounted() {
+    this.chess = this.$children[0];
+    this.chess.board.set({
+      movable: { events: { after: this.userPlay()} },
+    })
+  },
 });
